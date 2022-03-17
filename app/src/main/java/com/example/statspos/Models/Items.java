17782eb57faa270e3eea532a37bb18c9bc6e8e4e
Muhammad Ali <muhammadali058@gmail.com>
2022-03-17 11:31:38 +0500

    Some changes

---
+++ app/src/main/java/com/example/statspos/Models/Items.java
@@ -0,0 +1,37 @@
+package com.example.statspos.Models;
+
+import androidx.annotation.NonNull;
+
+public class Items {
+    String id, itemname;
+
+    public Items() {
+    }
+
+    public Items(String id, String itemname) {
+        this.id = id;
+        this.itemname = itemname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getItemname() {
+        return itemname;
+    }
+
+    public void setItemname(String itemname) {
+        this.itemname = itemname;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return itemname;
+    }
+}
